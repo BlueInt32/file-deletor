c5b3aab5a50b3a70d6c453aa2c398274ecfb588a
BlueInt32 <simon.budin@gmail.com>
2016-07-01 08:26:08 +0200

    Update minimum file size param to inclusive

--- FileDeletor/Program.cs
+++ FileDeletor/Program.cs
@@ -1,56 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FileDeletor
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			string folder = args[0];
-			int nDays = Convert.ToInt32(args[1]);
-			int ifBiggerThanInMB = Convert.ToInt32(args[2]);
-			string extension = args[3];
-			bool verbose = args[4] == "true";
-			if (!Directory.Exists(folder))
-			{
-				Console.WriteLine("Directory does not exist.");
-				return;
-			}
-			var files = Directory.EnumerateFiles(folder);
-			long spaceSaved = (long) 0.0;
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string folder = args[0];
+            int nDays = Convert.ToInt32(args[1]);
+            int ifBiggerThanInMB = Convert.ToInt32(args[2]);
+            string extension = args[3];
+            bool verbose = args[4] == "true";
+            if (!Directory.Exists(folder))
+            {
+                Console.WriteLine("Directory does not exist.");
+                return;
+            }
+            var files = Directory.EnumerateFiles(folder);
+            long spaceSaved = (long) 0.0;
 
-			foreach (var file in files.Where(f => f.EndsWith(extension)))
-			{
-				FileInfo fi = new FileInfo(file);
-				DateTime dt = fi.LastWriteTime;
-				var sizeInMB = (long)(fi.Length / 1000000.0);
+            foreach (var file in files.Where(f => f.EndsWith(extension)))
+            {
+                FileInfo fi = new FileInfo(file);
+                DateTime dt = fi.LastWriteTime;
+                var sizeInMB = (long)(fi.Length / 1000000.0);
 
-				bool deleteFile = dt < DateTime.Now.AddDays(-nDays) && sizeInMB > ifBiggerThanInMB;
+                bool fileIsTooBig = sizeInMB >= ifBiggerThanInMB;
+                bool fileIsTooOld = dt < DateTime.Now.AddDays(-nDays);
+                bool deleteFile = fileIsTooBig && fileIsTooOld;
 
-				if(verbose)
-					Console.WriteLine(deleteFile
-					? string.Format("{0} - {1} - {2} - {3}", file, dt.ToShortDateString(), sizeInMB, "deleted")
-					: string.Format("{0} - {1} - {2} - {3}", file, dt.ToShortDateString(), sizeInMB, "kept"));
+                if(verbose)
+                    Console.WriteLine(deleteFile
+                    ? string.Format("{0} - {1} - {2} - {3}", file, dt.ToShortDateString(), sizeInMB, "deleted")
+                    : string.Format("{0} - {1} - {2} - {3}", file, dt.ToShortDateString(), sizeInMB, "kept"));
 
-				if (deleteFile)
-				{
-					spaceSaved += sizeInMB;
-					File.Delete(file);
-				}
+                if (deleteFile)
+                {
+                    spaceSaved += sizeInMB;
+                    File.Delete(file);
+                }
 
-			}
+            }
 
-			if (verbose)
-			{
-				Console.WriteLine("");
-				Console.WriteLine(string.Format("{0} MB saved !", spaceSaved));
-				Console.Read();
-			}
-		}
-	}
+            if (verbose)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(string.Format("{0} MB saved !", spaceSaved));
+                Console.Read();
+            }
+        }
+    }
 }
